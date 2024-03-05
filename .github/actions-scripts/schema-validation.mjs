4488187bb0b5807bd1aeee08ddb0d0bcb840ccd2
Andre Dreyer <1818388+dr3dr3@users.noreply.github.com>
2024-03-05 03:41:32 +0000

    Fix: schema for groups in repositories.yml

--- .github/actions-scripts/schema-validation.mjs
+++ .github/actions-scripts/schema-validation.mjs
@@ -17,7 +17,7 @@ const repoSchemaCore = z.object({
 }).strict();
 
 const repoSchemaSolution = repoSchemaCore.extend({
-    groups: z.string(),
+    groups: z.array(z.string()).nonempty,
     route: z.string(),
 }).strict();
 
